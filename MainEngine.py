abde4a91b878c7d8a53fe163f75cc8f0295a88f5
PineMaster <shauryarajbansal@gmail.com>
2020-11-09 22:56:34 -0800

    com

--- MainEngine.py
+++ MainEngine.py
@@ -14,93 +14,19 @@ from datetime import datetime
 
 
 
-def parsecommands(command):
-    return str(command).split(",")
-
-def ClassifyType(section):
-    try:
-        Type = str(config[section]["Type"])
-    except KeyError:
-        print("Type does not exist in section " + section + " exiting...")
-        sys.exit()
-
-    if(Type == "Single"):
-        if(len(parsecommands(str(config[section]["commands"]))) > 1 ):
-            print("more than one command on certain section Exiting....")
-            sys.exit()
-        else:
-            return "Single"
-
-    elif(Type == "Multi"):
-        if(len(parsecommands(str(config[section]["commands"]))) < 2 ):
-            print("less thantwo command on certain section Exiting....")
-            sys.exit()
-        else:
-            return "Multi"
-    
-    elif(Type == "OneOrOther"):
-        if(len(parsecommands(str(config[section]["commands"]))) < 2 ):
-            print("less thantwo command on certain section Exiting....")
-            sys.exit()
-        else:
-            return "OneOrOther"
-    else:
-        print("Unknown type of type, exiting...")
-        sys.exit()
 
 
-def ClassifyTrueIf(section):
-    try:
-        TrueIf = str(config[section]["TrueIf"])
-    except KeyError:
-        print("TrueIf does not exist in section " + section + " exiting...")
-        sys.exit()
-
+def Main(section, Function):
     possibles = globals().copy()
     possibles.update(locals())
-    method = possibles.get(TrueIf)
-
-    if not method:
-        print("Unkown True If Method, exiting...")
+    method = possibles.get(Function)
+    if(not method):
+        print("Non defined function, exiting")
         sys.exit()
     else:
-        return TrueIf
-
-
-def Main(section, Type, TrueIf):
-    if(Type == "Single"):
-            possibles = globals().copy()
-            possibles.update(locals())
-            method = possibles.get(TrueIf)
-            result = method(parsecommands(str(config[section]["commands"]))[0], str(config[section]["result"]).encode('utf-8').decode('unicode_escape'))
-            return result
-    elif(Type == "Multi"):
-            result = True
-            possibles = globals().copy()
-            possibles.update(locals())
-            method = possibles.get(TrueIf)
-            for command in parsecommands(str(config[section]["commands"])):
-                result = method(command, str(config[section]["result"]).encode('utf-8').decode('unicode_escape'))
-                if result == False:
-                    return False
-            return True
-    elif(Type == "OneOrOther"):
-        result = False
-        possibles = globals().copy()
-        possibles.update(locals())
-        method = possibles.get(TrueIf)
-        for command in parsecommands(str(config[section]["commands"])):
-            result = method(command, str(config[section]["result"]).encode('utf-8').decode('unicode_escape'))
-            if result == True:
-                return True
-        return False
+        return method(section,config.options(section))
 
 def CheckStuff():
-    try:
-        TrueIf = str(config[section]["commands"])
-    except KeyError:
-        print("commands does not exist in section " + section + " exiting...")
-        sys.exit()
     try:
         TrueIf = str(config[section]["points"])
     except KeyError:
@@ -112,48 +38,14 @@ def CheckStuff():
         print("description does not exist in section " + section + " exiting...")
         sys.exit()
     try:
-        TrueIf = str(config[section]["result"]).encode('utf-8').decode('unicode_escape')
+        TrueIf = str(config[section]["Function"])
     except KeyError:
-        print("result does not exist in section " + section + " exiting...")
+        print("Function does not exist in section " + section + " exiting...")
         sys.exit()
 
-def EvalStr(s, raw=False):
-    r'''Attempt to evaluate a value as a Python string literal or
-       return s unchanged.
-
-       Attempts are made to wrap the value in one, then the 
-       form of triple quote.  If the target contains both forms
-       of triple quote, we'll just punt and return the original
-       argument unmodified.
-
-       Examples: (But note that this docstring is raw!)
-       >>> EvalStr(r'this\t is a test\n and only a \x5c test')
-       'this\t is a test\n and only a \\ test'
-
-       >>> EvalStr(r'this\t is a test\n and only a \x5c test', 'raw')
-       'this\\t is a test\\n and only a \\x5c test'
-    '''
-
-    results = s  ## Default returns s unchanged
-    if raw:
-       tmplate1 = 'r"""%s"""'
-       tmplate2 = "r'''%s'''"
-    else:
-       tmplate1 = '"""%s"""'
-       tmplate2 = "'''%s'''"
-
-    try:
-       results = eval(tmplate1 % s)
-    except SyntaxError:
-        try:
-            results = eval(tmplate2 %s)
-        except SyntaxError:
-            pass
-        return results
 
 
-
-file = '/home/krypt/Projects/ScoringEngine/config.ini'
+file = 'config.ini'
 config = configparser.RawConfigParser()
 config.read(file)
 
@@ -174,12 +66,11 @@ lost = 0
 Sections = config.sections()
 for section in Sections:
     CheckStuff()
-    Type = ClassifyType(section)
-    TrueIf =  ClassifyTrueIf(section)
     if(float(config[section]["points"]) > 0):
         vulnstotal += 1
         totalpoints += float(config[section]["points"])
-    result = Main(section, Type, TrueIf)
+    result = Main(section, str(config[section]["Function"]))
+    print(result)
     #string = str(config[section]["result"].encode('utf-8').decode('unicode_escape'))
     #print(ast.literal_eval(string))
     if(result == True):
